e6da30ca37e83e70ad7b37d04d549a0e6fe73392
Atikah <atikahnazir6@gmail.com>
2024-05-16 16:17:13 +0800

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "font-rubik": ["Rubik"],
+    },
+
+    extend: {
+      colors: {
+        "soft-blue": "hsl(231, 69%, 60%)",
+        "soft-Red": "hsl(0, 94%, 66%)",
+        "grayish-Blue": "hsl(229, 8%, 60%)",
+        "Very-Dark-Blue": "hsl(229, 31%, 21%)",
+      },
+    },
   },
   plugins: [],
 };
